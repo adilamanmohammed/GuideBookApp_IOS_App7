78996bb1285c346cd195ca48e89a1d62c1be57bf
Adil Aman Mohammed <adil.mohammed@okstate.edu>
2024-06-16 18:45:20 -0400

    App started

---
+++ Guide Book App/Guide Book App/Guide_Book_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guide_Book_AppApp.swift
+//  Guide Book App
+//
+//  Created by Adil Aman Mohammed on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Guide_Book_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
